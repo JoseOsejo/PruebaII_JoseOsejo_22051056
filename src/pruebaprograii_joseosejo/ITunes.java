c0812efcc7e1e31adc102c9a6bc28007bd21cba6
Jose Carlos Osejo <jcoq2003@gmail.com>
2022-03-21 09:55:50 -0600

    avance de prueba

--- src/pruebaprograii_joseosejo/ITunes.java
+++ src/pruebaprograii_joseosejo/ITunes.java
@@ -7,52 +7,51 @@ package pruebaprograii_joseosejo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Calendar;
 import javax.swing.JOptionPane;
 
 /**
  *
  * @author jcoq2
  */
-public class ITunes 
-{
+public class ITunes {
 
     //atributos de randomaccessfile para los archivos
-RandomAccessFile archivoSongs;
-RandomAccessFile archivoCodigo;
-RandomAccessFile archivoDownloads;
+    RandomAccessFile archivoSong;
+    RandomAccessFile archivoCodigo;
+    RandomAccessFile archivoDownloads;
 
-public ITunes(){
-    try{
-    File folder = new File("archivosItunes");
-    folder.mkdir();
-    
-    archivoSongs = new RandomAccessFile("archivosItunes/songs.itn","rw");
-    archivoCodigo = new RandomAccessFile("archivosItunes/codigos.itn","rw");
-    archivoDownloads = new RandomAccessFile("archivosItunes/downloads/itn","rw");
-    
-    setCodes();
-    }catch(IOException e){
-        JOptionPane.showMessageDialog(null, e);
+    public ITunes() {
+        try {
+            File folder = new File("archivosItunes");
+            folder.mkdir();
+
+            archivoSong = new RandomAccessFile("archivosItunes/songs.itn", "rw");
+            archivoCodigo = new RandomAccessFile("archivosItunes/codigos.itn", "rw");
+            archivoDownloads = new RandomAccessFile("archivosItunes/downloads/itn", "rw");
+
+            setCodes();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+
     }
-    
-}
-
- 
 
     private int getCodigo(long offset) throws IOException {
-        archivoSongs.seek(0);
-        int cancion = archivoSongs.readInt();
-        int descarga = archivoSongs.readInt();
+        archivoSong.seek(0);
+        int cancion = archivoSong.readInt();
+        int descarga = archivoSong.readInt();
         if (offset == 0) {
-            archivoSongs.seek(0);
-            archivoSongs.writeInt(cancion + 1);
+            archivoSong.seek(0);
+            archivoSong.writeInt(cancion + 1);
             return cancion;
         } else if (offset == 4) {
-            archivoSongs.seek(0);
-            archivoSongs.skipBytes(4);
-            archivoSongs.writeInt(descarga + 1);
+            archivoSong.seek(0);
+            archivoSong.skipBytes(4);
+            archivoSong.writeInt(descarga + 1);
             return descarga;
         } else {
             return 0;
@@ -60,29 +59,89 @@ public ITunes(){
     }
 
     private void setCodes() throws IOException {
-        if (archivoSongs.length() == 0) {
-            archivoSongs.writeInt(1);
-            archivoSongs.writeInt(1);
+        if (archivoSong.length() == 0) {
+            archivoSong.writeInt(1);
+            archivoSong.writeInt(1);
         }
     }
-    
 
-    
-    
-    public void downloadSong(int codeSong, String cliente){
-        
+    public void reviewSong(int code, int stars) throws IOException {
+        archivoSong.seek(0);
+        while (archivoSong.getFilePointer() < archivoSong.length()) {
+            long pos = archivoSong.getFilePointer();
+            if (archivoSong.readInt() == code) {
+                archivoSong.readUTF();
+                archivoSong.readUTF();
+                archivoSong.skipBytes(8);
+                int estrellas = archivoSong.readInt();
+                int reviews = archivoSong.readInt();
+
+                if (estrellas >= 0 && estrellas <= 5) {
+                    archivoSong.seek(pos);
+                    archivoSong.readUTF();
+                    archivoSong.readUTF();
+                    archivoSong.skipBytes(8);
+                    archivoSong.writeInt(stars + estrellas);
+                    archivoSong.writeInt(reviews + 1);
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
     }
-    
-    public void songs(String txtFile){
-        
+
+    public void addSong(String nombre, String cantante, double precio) throws IOException {
+        archivoSong.seek(archivoSong.length());
+        int codigo = getCodigo(0);
+        archivoSong.writeInt(codigo);
+        archivoSong.writeUTF(nombre);
+        archivoSong.writeUTF(cantante);
+        archivoSong.writeDouble(precio);
+        archivoSong.writeInt(0);
+        archivoSong.writeInt(0);
     }
-    
-    public void infoSong(int codeSong){
-        
+
+    public void downloadSong(int codigoSong, String cliente) throws IOException {
+
     }
 
+    public void songs(String txtFile) throws IOException {
+        String contenidoArchivo
+                = "";
+        File txt = new File(txtFile + ".txt");
+        FileWriter fw = new FileWriter(txt, false);
 
+        archivoSong.seek(0);
+        while (archivoSong.getFilePointer() < archivoSong.length()) {
+            int codigo = archivoSong.readInt();
+            String nombre = archivoSong.readUTF();
+            String cantante = archivoSong.readUTF();
+            double precio = archivoSong.readDouble();
+            int estrellas = archivoSong.readInt();
+            int reviews = archivoSong.readInt();
+            archivoSong.skipBytes(4);
+            double total = estrellas * reviews;
+            contenidoArchivo += " Codigo: " + codigo + " Nombre: " + nombre + " Cantante: " + cantante + " Rating: " + total + " Precio: " + precio + "\n";
+            fw.write(contenidoArchivo);
+        }
+        fw.flush();
+    }
 
+    public void infoSong(int codeSong) throws IOException {
+        archivoSong.seek(0);
+        while (archivoSong.getFilePointer() < archivoSong.length()) {
+            if (archivoSong.readInt() == codeSong) {
+                String nombre = archivoSong.readUTF();
+                String cantante = archivoSong.readUTF();
+                double precio = archivoSong.readDouble();
+                int estrellas = archivoSong.readInt();
+                int reviews = archivoSong.readInt();
+                int totalDeEstrellas = estrellas * reviews;
+                int descargas = archivoSong.readInt();
 
-    
+                System.out.println("Nombre: " + nombre + " Cantante: " + cantante + " Precio: " + precio + " Estrellas: " + totalDeEstrellas + " Reviews: " + reviews + " Downloads: " + descargas);
+            }
+        }
+    }
+
 }
---
+++ src/pruebaprograii_joseosejo/MainItunes.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaprograii_joseosejo;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author jcoq2
+ */
+public class MainItunes {
+    public static void main(String[] args) throws IOException {
+        int opcion = 0;
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
+        ITunes itune = new ITunes();
+        System.out.println("***Itunes***\n");
+        do {
+            System.out.print("1-Agregar Song\n2-Review Song\n3-Download Song\n4-Songs\n5-Info Song\n6-Salir\nIngrese una opcion: ");
+            opcion = sc.nextInt();
+            if (opcion == 1) {
+                System.out.println("Ingrese el nombre de la cancion: ");
+                String nombreCancion = sc.next();
+                System.out.println("Nombre del cantante: ");
+                String nombreCantante = sc.next();
+                System.out.println("Precio de la cancion");
+                double precioCancion = sc.nextDouble();
+                itune.addSong(nombreCancion, nombreCantante, precioCancion);
+            } else if (opcion == 2) {
+                System.out.println("Ingrese el codigo de la cancion : ");
+                int codigoCancion = sc.nextInt();
+                System.out.println("Ingrese cantidad de Estrellas: ");
+                int reviewEstrellas = sc.nextInt();
+                itune.reviewSong(codigoCancion, reviewEstrellas);
+            } else if (opcion == 3) {
+
+            } else if (opcion == 4) {
+                System.out.println("Direccion para el reporte: ");
+                String direccion = sc.next();
+                itune.songs(direccion);
+            } else if (opcion == 5) {
+                 System.out.println("Codigo de la cancion: ");
+                    int code = sc.nextInt();
+                    itune.infoSong(code);
+            }
+        } while (opcion != 6);
+    }
+}
